d68703f3c8e5434e8170cc8f4f767d7d0da056ef
Lauren Odalen <97255159+LaurenOdalen@users.noreply.github.com>
2022-03-24 07:55:21 -0600

    Add files via upload

---
+++ src/server/models/product.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Product title is required"],
+        minlength: [2, "Product title must be at least two characters"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"],
+        min: [0, "Price must be a positive number"]
+    },
+    description: {
+        type: String,
+        required: [true, "Product description is required"],
+        minlength: [3, "Product description must be at least three characters"]
+    }
+}, {timestamps: true});
+
+module.exports.Product = mongoose.model('Product', ProductSchema);
